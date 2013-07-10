5fbcd9e12a765b702db4ea594dfb6c335f8c3eab
Durga Malleswari <ubuntu@ip-172-31-22-101.us-west-2.compute.internal>
2013-07-10 19:15:03 +0000

    changed web jsto read from file

--- web.js
+++ web.js
@@ -1,5 +1,13 @@
-var express = require('express');
-
+//:wq
+//var express = require('express');
+/*var fs = require('fs');
+var content;
+var readFile = fs.readFileSync('./index.html', function read(err, data){
+if(err){
+throw err;}
+content = data;
+});
+console.log(content);
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
@@ -10,3 +18,40 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
+var fs = require("fs");
+var express = require("express");
+var fileName = "index.html";
+var data;
+fs.exists(fileName, function(exists)
+{
+if(exists)
+{
+fs.stat(fileName, function(err, stats)
+{
+//console.log(stats.size);
+fs.open(fileName, "r", function(error, fd) {
+var buffer = new Buffer(stats.size);
+fs.read(fd, buffer, 0, buffer.length, null, function(read, readBytes, buffer)
+{
+data = buffer.toString("utf8", 0, buffer.length);
+console.log(data);
+DisplayHtml(data);
+fs.close(fd);
+});
+});
+});
+}
+});
+var DisplayHtml = function(data)
+{
+var app = express.createServer(express.logger());
+app.get('/', function(request, response){
+response.send(data);
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+console.log("Listening on " + port);
+});
+}
+//console.log(data);
